89dd3110ff8ef2d3c1282741934fda089f3e351f
execgit <exec@iki.fi>
2016-07-23 10:58:33 +0300

    Fixed output format.
    Also minor fix to handling Scapy errors to get rid of
    NameError: global name 'Scapy_Exception' is not defined

--- runners/eicartest/eicartest.py
+++ runners/eicartest/eicartest.py
@@ -2,7 +2,7 @@ import sys
 import json
 import requests
 import tempfile
-from scapy.all import *
+from scapy.all import rdpcap
 from scapy.error import Scapy_Exception
 
 if len(sys.argv) != 2:
@@ -12,28 +12,35 @@ if len(sys.argv) != 2:
 try:
     r = requests.get(sys.argv[1])
     if r.status_code >= 400:
-        print json.dumps({"error": "File not found", "found": 0})
+        print json.dumps(
+            {"meta":
+             {"error":
+              "File not retrieved (status code {})".format(r.status_code),
+              "found": 0}
+             })
         sys.exit(2)
     with tempfile.NamedTemporaryFile() as temp:
         temp.write(r.content)
         temp.flush()
         packets = rdpcap(temp.name)
 except requests.exceptions.RequestException as e:
-    print json.dumps({"error": "Requests error", "found": 0})
+    print json.dumps({"meta": {"error": "Requests error", "found": 0}})
     sys.exit(3)
-except Scapy_Exception:
-    print json.dumps({"error": "Scapy error", "found": 0})
+except (Scapy_Exception, NameError):
+    print json.dumps({"meta": {"error": "Scapy error", "found": 0}})
     sys.exit(4)
 
 EICAR = 'X5O!P%@AP[4\\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*'
 
-out = list()
+found = 0
+
+meta = dict()
+data = list()
 for packet in packets:
     if EICAR in str(packet):
-        out.append(json.dumps({"found": 1, 
-                               "data": repr(packet.lastlayer().load)}))
+        found += 1
+        data.append({"payload": repr(str(packet)), "time": packet.time})
 
-if not out:
-    out.append({"found": 0})
+meta["found"] = found
 
-print json.dumps(out)
+print json.dumps({'meta': meta, 'data': data})
